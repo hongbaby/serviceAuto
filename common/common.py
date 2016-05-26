7e9dcffcdcfdbe59a9a7ba3a7a620bed934c693e
kerry.qiu <kerry.qiu@ef.com>
2016-05-26 18:07:06 +0800

    add test case for login

--- common/common.py
+++ common/common.py
@@ -1,8 +1,22 @@
+# coding: utf-8
+
+import os
+import csv
 import httplib
 from urlparse import urlparse
 import requests
 import json
 from myec.business_common.Utility import JSONResponse, ServiceRequestMethod
+
+
+def connect_and_send_requests(method, url_str, body):
+    url = urlparse(url_str)
+    hostname = url.hostname
+    try:
+        httplib.HTTPConnection(hostname)
+    except Exception, e:
+        print "Http connection issue: %s" % e
+    send_requests(method, url_str, body)
 
 
 def send_requests(method=ServiceRequestMethod.POST, url='', body={}):
@@ -12,22 +26,10 @@ def send_requests(method=ServiceRequestMethod.POST, url='', body={}):
         if response_status_code == 200:
             response_string = request.content
             json_response = json.loads(response_string)
-            print json_response
             get_error_code_and_description(json_response)
         else:
             print "The response status code is: %d" % response_status_code
             print request.reason
-
-
-def connect_and_send_requests(method, url_str, body):
-    url = urlparse(url_str)
-    hostname = url.hostname
-    try:
-        httplib.HTTPConnection(hostname)
-        send_requests(method, url_str, body)
-
-    except Exception, e:
-        print "Http connection issue: %s" % e
 
 
 def get_error_code_and_description(dic={}):
@@ -35,6 +37,30 @@ def get_error_code_and_description(dic={}):
         for key, value in dic.iteritems():
             if key == JSONResponse.HEADER:
                 print "key_name is: %s, value is: %s" % (key, value)
+                compare_error_code_and_error_description(value)
             if isinstance(dic[key], dict):
                 get_error_code_and_description(dic[key])
 
+
+def compare_error_code_and_error_description(dict):
+    error_code = dict[JSONResponse.ERRORCODE]
+    if error_code:
+        err_description = dict[JSONResponse.ERROR_DESCRIPTION]
+        verify_error_description(error_code, err_description)
+
+
+def verify_error_description(error_code, error_description):
+    current_path = os.getcwd()
+    thePath = current_path[:current_path.find("serviceAuto\\") + 12]
+
+    with open(os.path.dirname(thePath) + "\myec\data\error_code.csv", 'r') as csvfile:
+        csvfile.readline()
+        reader = csv.reader(csvfile)
+        for code, description in reader:
+            if int(code) == int(error_code):
+                try:
+                    assert error_description == description
+                except AssertionError:
+                    print "Assertion error: the expected error_description is: %s; " \
+                          "While the actual error description is: %s" % (description, error_description)
+                break
--- myec/tests/login.py
+++ myec/tests/login.py
@@ -34,8 +34,29 @@ class LoginRelated(unittest.TestCase):
 
         connect_and_send_requests(ServiceRequestMethod.POST, ServiceURLs.LOGIN_SERVICE_URL, body)
 
+    def test_login_with_invalid_account(self, username='stest12116', password='5'):
+        body = {
+            "serviceRequest": {
+                "header": HEADER,
+                "body": {
+                    "userName": username,
+                    "password": password
+                }
+            }
+        }
+
+        connect_and_send_requests(ServiceRequestMethod.POST, ServiceURLs.LOGIN_SERVICE_URL, body)
+
     def tearDown(self):
         pass
 
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(LoginRelated("test_with_valid_username_and_password"))
+    suite.addTest(LoginRelated("test_login_with_other_partner_account"))
+    suite.addTest(LoginRelated("test_login_with_invalid_account"))
+    return suite
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.TextTestRunner().run(suite())
